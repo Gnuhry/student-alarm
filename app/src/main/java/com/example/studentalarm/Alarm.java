3bafbfb1bb592fd17e80dd219a23330395737c8d
Gnuh <ytgnuh@gmail.com>
2020-10-24 11:49:00 +0200

    [#08] Adding option for alarm

--- app/src/main/java/com/example/studentalarm/Alarm.java
+++ app/src/main/java/com/example/studentalarm/Alarm.java
@@ -4,6 +4,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.provider.AlarmClock;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -13,7 +14,8 @@ public class Alarm {
 
     /**
      * create an alarm, which going to trigger the alarmreceiver class
-     * @param time the time, the receiver should be triggerd
+     *
+     * @param time    the time, the receiver should be triggerd
      * @param context context to show the toast
      */
     public void setAlarm(Calendar time, Context context) {
@@ -24,15 +26,27 @@ public class Alarm {
             assert alarmManager != null;
             alarmManager.set(AlarmManager.RTC_WAKEUP, time.getTimeInMillis(), pendingIntent);
             Toast.makeText(context, "Alarm is set", Toast.LENGTH_SHORT).show(); //TODO show Time
-            Log.d("ALARM", "Set alarm to "+time.getTimeInMillis());
+            Log.d("ALARM", "Set alarm to " + time.getTimeInMillis());
         } else {
             Log.e("ALARM", "Wrong Date is set for alarm. Date is before current date");
         }
-//            Intent intent=new Intent(AlarmClock.ACTION_SET_ALARM);
-//            intent.putExtra(AlarmClock.EXTRA_HOUR,hour);
-//            intent.putExtra(AlarmClock.EXTRA_MINUTES,minute);
-//            intent.putExtra(AlarmClock.EXTRA_SKIP_UI,true);
-//            context.startActivity(intent);
+    }
+
+    /**
+     * create an alarm in the phone alarm app
+     *
+     * @param hour    hour when the alarm should trigger
+     * @param minute  minutes when the alarm should trigger
+     * @param context context to show the toast
+     */
+    public void setPhoneAlarm(int hour, int minute, Context context) {
+        if (hour <= 24 && minute <= 60) {
+            Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+            intent.putExtra(AlarmClock.EXTRA_HOUR, hour);
+            intent.putExtra(AlarmClock.EXTRA_MINUTES, minute);
+            intent.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+            context.startActivity(intent);
+        }
     }
 
 
