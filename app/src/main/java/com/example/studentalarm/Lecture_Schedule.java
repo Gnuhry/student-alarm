de4fb8647c0d698cfc9bd5df30c1eb1a8ac439b9
Gnuh <ytgnuh@gmail.com>
2020-11-02 15:19:50 +0100

    [#02] Adding weekview to display lecture

--- app/src/main/java/com/example/studentalarm/Lecture_Schedule.java
+++ app/src/main/java/com/example/studentalarm/Lecture_Schedule.java
@@ -1,14 +1,23 @@
 package com.example.studentalarm;
 
+import android.graphics.Color;
+import android.util.Log;
+
+import com.alamkanak.weekview.WeekViewDisplayable;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.TimeZone;
 
 public class Lecture_Schedule {
     private TimeZone timezone;
-    private List<Lecture> lecture;
-    //TODO Soll es möglich sein, ereignsise hinzuzufügen, wenn es einen Import gibt?
+    private final List<Lecture> lecture;
 
     /**
      * Create an empty lecture schedule
@@ -30,7 +39,7 @@ public class Lecture_Schedule {
         timezone = TimeZone.getDefault();//TODO Getter TimeZone iCalendar
     }
 
-    public void addLecture(Lecture lecture){
+    public void addLecture(Lecture lecture) {
         this.lecture.add(lecture);
     }
 
@@ -49,9 +58,11 @@ public class Lecture_Schedule {
     /**
      * inner class to represent the lecture information
      */
-    public class Lecture {
-        private String docent, location, name;
-        private Date start, end;
+    public static class Lecture implements WeekViewDisplayable<Lecture> {
+        private final String docent, location, name;
+        private final Date start, end;
+        private static int id = 1;
+        private final int color;
 
         public Lecture(String name, String docent, String location, Date start, Date end) {
             this.name = name;
@@ -59,46 +70,41 @@ public class Lecture_Schedule {
             this.location = location;
             this.start = start;
             this.end = end;
+            this.color = Color.RED;
         }
 
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
+        public Lecture(String name, String docent, String location, Date start, Date end, int color) {
             this.name = name;
-        }
-
-        public String getDocent() {
-            return docent;
-        }
-
-        public void setDocent(String docent) {
             this.docent = docent;
-        }
-
-        public String getLocation() {
-            return location;
-        }
-
-        public void setLocation(String location) {
             this.location = location;
-        }
-
-        public Date getStart() {
-            return start;
-        }
-
-        public void setStart(Date start) {
             this.start = start;
-        }
-
-        public Date getEnd() {
-            return end;
-        }
-
-        public void setEnd(Date end) {
             this.end = end;
+            this.color = color;
+        }
+
+        @NotNull
+        @Override
+        public WeekViewEvent<Lecture> toWeekViewEvent() { //TODO add Docent to Text
+            WeekViewEvent.Style.Builder builder = new WeekViewEvent.Style.Builder();
+            builder.setBackgroundColor(color);
+
+            Calendar startCal = new GregorianCalendar();
+            startCal.setTime(this.start);
+            Log.d("Lecture - Start Date", startCal.toString());
+
+            Calendar endCal = new GregorianCalendar();
+            endCal.setTime(this.end);
+            Log.d("Lecture - End Date", endCal.toString());
+
+            WeekViewEvent.Builder<Lecture> erg = new WeekViewEvent.Builder<>(this);
+            erg.setTitle(this.name);
+            erg.setStartTime(startCal);
+            erg.setEndTime(endCal);
+            if (this.location != null)
+                erg.setLocation(this.location);
+            erg.setStyle(builder.build());
+            erg.setId(id++);
+            return erg.build();
         }
     }
 }
