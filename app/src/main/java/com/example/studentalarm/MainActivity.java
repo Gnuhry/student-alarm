8b9c52001e9fb747f2cb5bfc7b746d70f1764698
Gnuh <ytgnuh@gmail.com>
2020-11-02 15:19:50 +0100

    [#02] Adding weekview to display lecture

--- app/src/main/java/com/example/studentalarm/MainActivity.java
+++ app/src/main/java/com/example/studentalarm/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.studentalarm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.alamkanak.weekview.WeekView;
+
+import java.text.SimpleDateFormat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    WeekView.SimpleAdapter<Lecture_Schedule.Lecture> adapter;
+    SimpleDateFormat format=new SimpleDateFormat("EEE dd.MM");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WeekView weekview = findViewById(R.id.weekView);
+        adapter = new WeekView.SimpleAdapter<>();
+        weekview.setAdapter(adapter);
+        weekview.setTimeFormatter(hour -> {
+            if (hour < 10)
+                return "0" + hour + " h";
+            return "" + hour + " h";
+        });
+        weekview.setDateFormatter(date->format.format(date.getTime()));
+
+        Thread x = new Thread(() -> {
+            Lecture_Schedule l = new Lecture_Schedule(new ICS("http://vorlesungsplan.dhbw-mannheim.de/ical.php?uid=7758001", true));
+            adapter.submit(l.getLecture());
+        });
+        x.start();
     }
 }
