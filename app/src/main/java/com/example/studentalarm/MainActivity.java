bd6c8fafa00aaa1a45b17a11602503a7572da52c
Gnuh <ytgnuh@gmail.com>
2020-11-03 21:58:57 +0100

    [#02] Adding bottom navigation menue and fragments

--- app/src/main/java/com/example/studentalarm/MainActivity.java
+++ app/src/main/java/com/example/studentalarm/MainActivity.java
@@ -2,35 +2,54 @@ package com.example.studentalarm;
 
 import android.os.Bundle;
 
-import com.alamkanak.weekview.WeekView;
-
-import java.text.SimpleDateFormat;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
-    WeekView.SimpleAdapter<Lecture_Schedule.Lecture> adapter;
-    SimpleDateFormat format = new SimpleDateFormat("EEE dd.MM");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WeekView weekview = findViewById(R.id.weekView);
-        adapter = new WeekView.SimpleAdapter<>();
-        weekview.setAdapter(adapter);
-        weekview.setTimeFormatter(hour -> {
-            if (hour < 10)
-                return "0" + hour + " h";
-            return "" + hour + " h";
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+
+        openFragment(new AlarmFragment());
+
+
+
+        bottomNav.setOnNavigationItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.alarm) {
+                openFragment(new AlarmFragment());
+                return true;
+            } else if (itemId == R.id.lecture) {
+                openFragment(new LectureFragment());
+                return true;
+            } else if (itemId == R.id.way) {
+                openFragment(new WayFragment());
+                return true;
+            } else if (itemId == R.id.setting) {
+                openFragment(new SettingsFragment());
+                return true;
+            }
+
+
+            return false;
         });
-        weekview.setDateFormatter(date -> format.format(date.getTime()));
-        adapter.submit(Lecture_Schedule.Load(this).getAllLecture());
-        Thread x = new Thread(() -> {
-            Lecture_Schedule l = Import.ICSImport("http://vorlesungsplan.dhbw-mannheim.de/ical.php?uid=7758001", this);
-            adapter.submit(l.getAllLecture());
-        });
-        x.start();
+
+    }
+
+    void openFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
     }
 }
