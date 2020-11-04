6f055b48f7922b317c38d94430187e42c2c10c3e
Gnuh <ytgnuh@gmail.com>
2020-11-04 14:41:54 +0100

    Rename

--- app/src/main/java/com/example/studentalarm/SchoolFragment.java
+++ app/src/main/java/com/example/studentalarm/SchoolFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link WayFragment#newInstance} factory method to
+ * Use the {@link SchoolFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class WayFragment extends Fragment {
+public class SchoolFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class WayFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public WayFragment() {
+    public SchoolFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class WayFragment extends Fragment {
      * @return A new instance of fragment WayFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static WayFragment newInstance(String param1, String param2) {
-        WayFragment fragment = new WayFragment();
+    public static SchoolFragment newInstance(String param1, String param2) {
+        SchoolFragment fragment = new SchoolFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class WayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_way, container, false);
+        return inflater.inflate(R.layout.fragment_school, container, false);
     }
 }
