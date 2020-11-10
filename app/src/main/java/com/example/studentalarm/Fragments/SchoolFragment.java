be63a94aae50f8e26295c57dea6e8c4b85abe281
Gnuh <ytgnuh@gmail.com>
2020-11-10 23:12:01 +0100

    [#02, #23, #24} Adding/Updating new UIs

--- app/src/main/java/com/example/studentalarm/Fragments/SchoolFragment.java
+++ app/src/main/java/com/example/studentalarm/Fragments/SchoolFragment.java
@@ -1,4 +1,4 @@
-package com.example.studentalarm;
+package com.example.studentalarm.Fragments;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -11,11 +11,13 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.EditText;
 
+import com.example.studentalarm.AlarmManager;
+import com.example.studentalarm.R;
+
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
 public class SchoolFragment extends Fragment {
-
-    private String m_Text;
 
     public SchoolFragment() {
     }
@@ -35,8 +37,15 @@ public class SchoolFragment extends Fragment {
         return view;
     }
 
+    /**
+     * Init Number Dialog
+     *
+     * @param context context of the application
+     * @param title   title of the number dialog
+     * @param key     key of preference
+     */
     private void NumberDialog(Context context, String title, String key) {
-        SharedPreferences preferences = context.getSharedPreferences("ALARM", Context.MODE_PRIVATE);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setTitle(title);
         View viewInflated = LayoutInflater.from(getContext()).inflate(R.layout.number_input, (ViewGroup) getView(), false);
@@ -49,7 +58,7 @@ public class SchoolFragment extends Fragment {
             int value = Integer.parseInt(input.getText().toString());
             if (preferences.getInt(key, 0) != value) {
                 preferences.edit().putInt(key, value).apply();
-                //TODO check alarm
+                AlarmManager.UpdateNextAlarm(context);
             }
         });
         builder.setNegativeButton(android.R.string.cancel, (dialog, which) -> dialog.cancel());
---
+++ app/src/main/java/com/example/studentalarm/Receiver/BootReceiver.java
@@ -0,0 +1,27 @@
+package com.example.studentalarm.Receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import com.example.studentalarm.AlarmManager;
+import com.example.studentalarm.Import.Import;
+
+import androidx.preference.PreferenceManager;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    /**
+     * triggered if phone has booted
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            SharedPreferences preferences= PreferenceManager.getDefaultSharedPreferences(context);
+            if(preferences.getBoolean("AUTO_IMPORT",false))
+                new Import().SetTimer(context);
+            AlarmManager.SetNextAlarm(context);
+        }
+    }
+}
