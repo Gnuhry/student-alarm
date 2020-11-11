1738adfc64ff8783e246885d07265d4314bf8065
Gnuh <ytgnuh@gmail.com>
2020-11-11 20:29:35 +0100

    [#25] Catch permissions missing

--- app/src/main/java/com/example/studentalarm/Fragments/AlarmFragment.java
+++ app/src/main/java/com/example/studentalarm/Fragments/AlarmFragment.java
@@ -7,12 +7,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.studentalarm.Import.Lecture_Schedule;
 import com.example.studentalarm.R;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 
@@ -36,6 +39,14 @@ public class AlarmFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_alarm, container, false);
         if (getContext() == null) return view;
+        if(NotificationManagerCompat.from(getContext()).areNotificationsEnabled()){
+            new MaterialAlertDialogBuilder(getContext())
+                    .setTitle(R.string.notification_permission_missing)
+                    .setMessage(R.string.notification_permission_are_missing_without_them_the_alarm_will_not_work_properly)
+                    .setPositiveButton(R.string.ok,null)
+                    .setCancelable(true)
+                    .show();
+        }
         long time = PreferenceManager.getDefaultSharedPreferences(getContext()).getLong("ALARM_TIME", 0);
         if (time != 0 && time > Calendar.getInstance().getTimeInMillis()) {
             TextView txVTimer = view.findViewById(R.id.txVCountdown);
