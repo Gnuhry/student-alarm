1e8e0c031066f3f7900e0a5d29dfbe2bf4f9a53e
Flix <felix.koren@gmail.com>
2020-12-19 13:27:33 +0100

    parser performance boost

--- app/src/main/java/com/example/studentalarm/imports/dhbwMannheim/DhbwCourses.java
+++ app/src/main/java/com/example/studentalarm/imports/dhbwMannheim/DhbwCourses.java
@@ -17,51 +17,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.List;
-/*
-public class DhbwCourses implements Serializable {
-
-    private final String FILENAME ="DHBWCOURSES";
-    private List<CourseCategory> courseCategorys;
-
-    public DhbwCourses(){}
-
-    public DhbwCourses(List<CourseCategory> courseCategorys){
-        this.courseCategorys=courseCategorys;
-    }
-
-    public List<CourseCategory> getCourseCategorys() {
-        return courseCategorys;
-    }
-
-    public void save (@NonNull Context context){
-        FileOutputStream fos;
-        try {
-            fos = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(this);
-            oos.close();
-            fos.close();
-            Log.d("SAVE", "Save Coursedata in DHBWCOURSES: SUCCESS");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void load (@NonNull Context context) {
-        try {
-            FileInputStream fis = context.openFileInput(FILENAME);
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            this.courseCategorys = ((DhbwCourses) ois.readObject()).getCourseCategorys();
-            Log.d("LOAD", "Loaded Coursedata from " + FILENAME + ": SUCCESS");
-            fis.close();
-            ois.close();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-}
-*/
-
 public class DhbwCourses{
     private static final String FILENAME ="DHBWCOURSES";
     private static void save (@NonNull Context context,@NonNull List<CourseCategory> courseCatergories){
@@ -120,24 +75,3 @@ public class DhbwCourses{
         public List<CourseCategory>save;
     }
 }
-/*String[] files = getContext().fileList();
-            List<String> list = Arrays.asList(files);
-            Log.d("Check", "Is DHBWCOURSES in: "+list);
-            if (!list.contains("DHBWCOURSES")){
-                Log.i(LOG, "Import from WEB");
-                dhbwCourseImport();
-            }else{
-                Log.i(LOG, "Import from DHBWCOURSES");
-                dhbwCourses.load(getContext());
-                if (dhbwCourses==null||dhbwCourses.getCourseCategorys()==null){
-                    dhbwCourseImport();
-                }
-            }
-            if (dhbwCourses==null||dhbwCourses.getCourseCategorys()==null)
-                return;
-
-
-                private void dhbwCourseImport() {
-
-    }
- */
