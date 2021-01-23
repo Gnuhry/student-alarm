e22565530566748df4ee62cdc4ba47219c2b5dec
Gnuh <ytgnuh@gmail.com>
2021-01-23 13:39:04 +0100

    fix theme

--- app/src/main/java/com/example/studentalarm/MainActivity.java
+++ app/src/main/java/com/example/studentalarm/MainActivity.java
@@ -5,17 +5,18 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.NavigationUI;
-import androidx.preference.PreferenceManager;
-
 import com.example.studentalarm.receiver.NetworkReceiver;
 import com.example.studentalarm.save.PreferenceKeys;
 import com.example.studentalarm.ui.dialog.ImportDialog;
 import com.example.studentalarm.ui.fragments.SettingsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import androidx.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +33,20 @@ public class MainActivity extends AppCompatActivity {
         if (navHostFragment != null)
             NavigationUI.setupWithNavController((BottomNavigationView) findViewById(R.id.bottomNav), navHostFragment.getNavController());
         PreferenceKeys.setDefault(this);
+
+        int mode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+        switch (PreferenceManager.getDefaultSharedPreferences(this).getString(PreferenceKeys.THEME, "")) {
+            case "Default":
+                mode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+                break;
+            case "Light":
+                mode = AppCompatDelegate.MODE_NIGHT_NO;
+                break;
+            case "Dark":
+                mode = AppCompatDelegate.MODE_NIGHT_YES;
+                break;
+        }
+        AppCompatDelegate.setDefaultNightMode(mode);
 
         String lan = PreferenceManager.getDefaultSharedPreferences(this).getString(PreferenceKeys.LANGUAGE, null);
         if (lan != null && !lan.equals(PreferenceKeys.defaultLanguage(this)))
